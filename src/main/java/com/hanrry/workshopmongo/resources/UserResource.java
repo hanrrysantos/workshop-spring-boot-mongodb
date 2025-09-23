fcccdaca32035fa19413f6f11787875c892dbd4b
Hanrry Santos <hanrry.jsantos@gmail.com>
2025-09-23 08:21:46 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/hanrry/workshopmongo/resources/UserResource.java
+++ src/main/java/com/hanrry/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.hanrry.workshopmongo.resources;
 
+import com.hanrry.workshopmongo.domain.Post;
 import com.hanrry.workshopmongo.domain.User;
 import com.hanrry.workshopmongo.dto.UserDTO;
 import com.hanrry.workshopmongo.services.UserService;
@@ -53,4 +54,11 @@ public class UserResource {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping(value="/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+    }
+
 }
